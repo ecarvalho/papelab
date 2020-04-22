d7a6cd3626bbd5ba458ce8cec5228efff30e3ff2
Edson Carvalho <edsonlcj@gmail.com>
2020-04-22 23:16:26 +0200

    Updates

---
+++ precache-manifest.aac2b1cf1ecb209c503ec0a0d7b6e75f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe33c048bce2a509334ab54b922e6a4f",
+    "url": "/papelab/index.html"
+  },
+  {
+    "revision": "e3df068b62f8bf5565ed",
+    "url": "/papelab/static/css/main.461c0bc4.chunk.css"
+  },
+  {
+    "revision": "ffbd8ff56dc768552bbc",
+    "url": "/papelab/static/js/2.26c56aa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/papelab/static/js/2.26c56aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3df068b62f8bf5565ed",
+    "url": "/papelab/static/js/main.df8ac4a4.chunk.js"
+  },
+  {
+    "revision": "7ff27401a750bb857b5b",
+    "url": "/papelab/static/js/runtime-main.543b446d.js"
+  },
+  {
+    "revision": "bfbff3defa724ab7ea60c4aabb46fe19",
+    "url": "/papelab/static/media/heart.bfbff3de.svg"
+  },
+  {
+    "revision": "46adb08b31df8d923b6b4dab191f05f5",
+    "url": "/papelab/static/media/heart.border.46adb08b.svg"
+  },
+  {
+    "revision": "5b41c36bc7aafd00740a5e8084947e1b",
+    "url": "/papelab/static/media/star.5b41c36b.svg"
+  },
+  {
+    "revision": "51151376f97183369734e22ed4acefa7",
+    "url": "/papelab/static/media/star.border.51151376.svg"
+  }
+]);
